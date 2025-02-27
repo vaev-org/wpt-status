05ba046bbb5f4907f877c178eb24f6b451f126e3
Lou ! <lou@habert.me>
2025-02-27 16:17:19 +0100

    higher canvas and reduce clone depth

--- runWPT.py
+++ runWPT.py
@@ -15,14 +15,14 @@ def run(command):
 print("Preparing the environment...")
 #clone latest target repository
 if not local:
-    run(f"git clone {target_repository} paper-muncher")
+    run(f"git clone {target_repository} paper-muncher --depth 1")
 
     #install the target
     run("cd paper-muncher && ./ck tools setup") #cd ing because ck does not handle relative paths correctly
     run("cd paper-muncher && ./ck package install --mixins=release --prefix=$HOME/.local/opt/paper-muncher")
 
     #clone latest WPT repository
-    run(f"git clone {wpt_repository} wpt")
+    run(f"git clone {wpt_repository} wpt --depth 1")
 
     #generate WPT hosts
     run("./wpt/wpt make-hosts-file | sudo tee -a /etc/hosts")
