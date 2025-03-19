002d71487a83c931bef60736324a6b4204834bb8
Lou ! <lou@habert.me>
2025-03-19 15:58:58 +0100

    remove problematic terms and merge include list with the one in the main repo

--- processResults.py
+++ processResults.py
@@ -17,9 +17,35 @@ print("Processing the results...")
 #retreive last log
 
 wpt_repository = "https://github.com/vaev-org/wpt.git"
+includedTests = "https://raw.githubusercontent.com/odoo/paper-muncher/refs/heads/main/meta/wpt/includelist"
 
-def loadWhiteList():
-    f = open("./wpt-whitelist", "r")
+def download_file(url, save_path):
+    """
+    Downloads a file from a given URL and saves it to the specified path.
+
+    Args:
+        url (str): The URL of the file to download.
+        save_path (str): The local path where the file should be saved.
+    """
+    try:
+        response = requests.get(url, stream=True)  # stream=True for large files
+        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192): #8192 is a good chunk size
+                if chunk:  # filter out keep-alive new chunks
+                    file.write(chunk)
+
+        print(f"File downloaded successfully to: {save_path}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading file: {e}")
+    except IOError as e:
+        print(f"Error saving file: {e}")
+
+def loadIncluded():
+    download_file(includedTests, "./includedTests")
+    f = open("./includedTests", "r")
     whitelist = f.read()
     f.close()
     print(f"WHITE: {whitelist}")
@@ -88,9 +114,9 @@ flattened = {"results":[]}
 for res in results:
     flattened["results"] = flattened["results"] + res['results']
 
-whitelist = loadWhiteList()
+included = loadIncluded()
 
-structured = processWPTdata(whitelist, flattened)
+structured = processWPTdata(included, flattened)
 
 #append it to ours
 saveResults(structured)
