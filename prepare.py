fbab1cc1cf47e60e832834eb4e1fb67a1f813849
Lou ! <46999366+Louciole@users.noreply.github.com>
2025-06-02 15:05:07 +0200

    fix build

--- prepare.py
+++ prepare.py
@@ -11,7 +11,7 @@ run(f"git clone {target_repository} paper-muncher --depth 1")
 
 #install the target
 run("cd paper-muncher && ./ck tools setup") #cd ing because ck does not handle relative paths correctly
-run("cd paper-muncher && ./ck install --mixins=release --prefix=$HOME/.local/opt/paper-muncher")
+run("cd paper-muncher && ./ck install --mixins=release --prefix=$HOME/.local/opt/paper-muncher paper-muncher")
 
 #clone latest WPT repository
 run(f"git clone {wpt_repository} wpt --depth 1")
